eafdc3134a200f3ff0a60ff6d7de5a3b1d9d0260
Deebo415 <dobbs.dobbs148@gmail.com>
2025-05-08 07:11:29 -0400

    Clean repository with only essential files for TRCStatR app

--- tests/testthat.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-library(shiny)
-
-# Load application files
-source("../R/dependencies.R")
-ensure_packages_installed(c("testthat", "mockery", "DBI"))
-
-# Run all tests
-test_dir("testthat", reporter = "summary")
